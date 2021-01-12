9cf60ceeb8cd9464637bae93e94ff6cc391d93a1
Yevhen Bohaienko <yevhen@1touch.io>
2021-01-12 04:48:06 +0200

    fix

--- src/components/DataSources.vue
+++ src/components/DataSources.vue
@@ -15,14 +15,28 @@
                 <td class="vert-aligned">DropBox</td>
                 <td>
                     <label>
-                        <input type="text" name="token" size="70%" @keyup.enter="enableCrawl" v-model="accessToken"
+                        <input type="text" name="token" size="70%" @keyup.enter="enableCrawl" v-model="dpxAccessToken"
                                placeholder="XLSKbV26LAFFBXsftNaKl2gBI6d5utRAk8bmszo2TFoay229WrqQzi08ZYxgqO86">
                     </label>
                 </td>
                 <td class="vert-aligned">
-                    <button v-on:click="enableCrawl">Start crawl</button>
+                    <button v-on:click="enableCrawl">Start scan</button>
                 </td>
                 <td class="vert-aligned">{{statusMessage}}</td>
+            </tr>
+            <tr>
+                <td class="vert-aligned">Google Drive</td>
+                <td>
+                    <label>
+                        <input type="text" name="token" size="70%" @keyup.enter="enableCrawl" v-model="gDaccessToken"
+                               placeholder="AIzaSyD0QT3Gv1va8FOSY0wQW028GKOdjgZrsp8">
+                    </label>
+                </td>
+                <td class="vert-aligned">
+                    <button v-on:click="enableCrawl">Start scan</button>
+                </td>
+                <!--                <td class="vert-aligned">{{statusMessage}}</td>-->
+                <td class="vert-aligned">STOPPED</td>
             </tr>
             </tbody>
         </table>
@@ -36,13 +50,17 @@
             return {
                 statusMessage: null,
                 actionStatus: null,
-                accessToken: ''
+                dpxAccessToken: ''
             };
         },
         created: function () {
             setInterval(() => {
                 axios
-                    .get('http://localhost:8079/api/dropbox/status')
+                    .get('http://localhost:8079/api/dropbox/status', {
+                        headers: {
+                            "Content-Type": "application/json"
+                        }
+                    })
                     .then(res => {
                         this.statusMessage = res.data.statusMessage;
                     })
@@ -54,7 +72,7 @@
                     .post(
                         'http://localhost:8079/api/dropbox/start',
                         {
-                            "token": this.accessToken
+                            "token": this.dpxAccessToken
                         },
                     )
             }
